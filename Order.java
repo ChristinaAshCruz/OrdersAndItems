68b0d336c711369ee6c9e07d0c6fa4cd0f0df2b3
ChristinaAshCruz <christina.ash.cruz@gmail.com>
2023-02-16 18:08:58 -0800

    Compiled + created order1

--- Order.java
+++ Order.java
@@ -6,7 +6,12 @@ public class Order {
     public String name;
     public Double total;
     public boolean ready;
-    public ArrayList<Item> items;
+    public ArrayList<Object> items;
 
-    
+    public Order(String name, double total, boolean ready, ArrayList<Object> items){
+        this.name = name;
+        this.total = total;
+        this.ready = ready;
+        this.items = items;
+    }
 }
--- TestOrders.java
+++ TestOrders.java
@@ -3,12 +3,20 @@ import java.util.ArrayList;
 public class TestOrders {
     public static void main(String[] args) {
         
-        Order order1 = new Order();
-        Item item1 = new Item();
-
         // Menu items
-    
+        Item item1 = new Item("Ube Latte", 4.25);
+        Item item2 = new Item("Double Espresso", 3.25);
+        Item item3 = new Item("Almondmilk Flat White", 4.25);
+        Item item4 = new Item("Vietnamese Iced Coffee", 4.00);
+        
         // Order variables -- order1, order2 etc.
+        ArrayList<Object> order1Items = new ArrayList<>();
+        order1Items.add(item1);
+
+
+        Order order1 = new Order("Jane", 4.25, false, order1Items);
+
+    
     
         // Application Simulations
         // Use this example code to test various orders' updates
