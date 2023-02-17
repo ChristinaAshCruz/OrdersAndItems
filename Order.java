15c248f3d81610745a9ec1035cc70722e1d763c8
ChristinaAshCruz <christina.ash.cruz@gmail.com>
2023-02-16 17:45:52 -0800

    establishing classes + fields

---
+++ Order.java
@@ -0,0 +1,12 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class Order {
+    
+    public String name;
+    public Double total;
+    public boolean ready;
+    public ArrayList<Item> items;
+
+    
+}
